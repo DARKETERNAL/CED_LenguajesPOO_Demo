ba829b304a087042ca9ecd35fa5fe5c5b154f59d
Andres Perez Campillo <tomailsandstuff@gmail.com>
2020-04-01 10:42:14 -0500

    First commit
    Base demo code

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody myRigidbody;
+
+    public float jumpForce;
+
+    private bool canJump = true;
+
+    [SerializeField]
+    private float jumpCooldown = 0.3F;
+
+    [SerializeField]
+    private ParticleSystem fallParticleSystem;
+
+    [SerializeField]
+    private ParticleSystem hitParticleSystem;
+
+    private int jumpCount;
+
+    private bool playFallPS;
+
+    [SerializeField]
+    private float moveSpeed = 1F;
+
+    public int JumpCount { get { return jumpCount; } }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void DoJump()
+    {
+        if (canJump)
+        {
+            jumpCount += 1;
+            canJump = false;
+            myRigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetAxis("Jump") != 0F)
+        {
+            DoJump();
+        }
+
+        float hVal = Input.GetAxis("Horizontal");
+
+        if (hVal != 0F)
+        {
+            transform.Translate(
+                transform.right * hVal * moveSpeed * Time.deltaTime);
+        }
+    }
+
+    private void ResetCanJump()
+    {
+        canJump = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer.Equals(
+            LayerMask.NameToLayer("Ground")))
+        {
+            if (playFallPS)
+            {
+                if (fallParticleSystem != null)
+                {
+                    fallParticleSystem.Play();
+                }
+
+                ResetCanJump();
+            }
+            else
+            {
+                playFallPS = true;
+            }
+        }
+        else if (collision.gameObject.layer.Equals(
+            LayerMask.NameToLayer("JumpCounter")))
+        {
+            ParticleSystem psInstance = Instantiate<ParticleSystem>(
+                hitParticleSystem,
+                collision.contacts[0].point,
+                Quaternion.identity);
+
+            Destroy(psInstance.gameObject, 0.6F);
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController player;
+
+    [SerializeField]
+    private Text text;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        text.text = player.JumpCount.ToString();
+    }
+}
