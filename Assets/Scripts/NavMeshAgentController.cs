9af8182d0d3a8599b372984c9bd277028b24e56a
Andres Perez Campillo <tomailsandstuff@gmail.com>
2020-04-22 11:35:08 -0500

    NavMesh example

---
+++ Assets/Scripts/NavMeshAgentController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshAgentController : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask walkableLayer;
+
+    protected NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // detectar posición del mouse
+            // tirar raycast hacia la pantalla
+            // si encuentro suelo, mover al punto.
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, walkableLayer))
+            {                
+                agent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/NavmeshAIController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class NavmeshAIController : NavMeshAgentController
+{
+    [SerializeField]
+    private Transform playerTransform;
+
+    [SerializeField]
+    private float distanceToChase = 150F;
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        if ((playerTransform.position - transform.position).magnitude <= distanceToChase)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(playerTransform.position);
+        }
+        else
+        {
+            agent.isStopped = true;
+        }
+    }
+}
